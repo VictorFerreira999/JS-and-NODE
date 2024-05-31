0ab6d12ef6bfb364f4996ecea29d6bc8fd9e16b5
Victor Ferreira <victorferreira9990@gmail.com>
2024-05-31 20:42:41 -0300

    gets e sets

--- poo/criandoObjeto.js
+++ poo/criandoObjeto.js
@@ -18,3 +18,28 @@ function Produto(nome, preco, desc){
 const p1 = new Produto('Caneta', 7.99, 0.15)
 const p2 = new Produto('Notebook', 2998.99, 0.25)
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+//Função factory
+function criarFuncionario(nome, salarioBaase, faltas) {
+   return{
+      nome,
+      salarioBaase,
+      faltas,
+      getSalario(){
+         return(salarioBaase / 30 * (30 - faltas))
+      }
+   }
+}
+
+const f1 = criarFuncionario('Joao', 7980, 4)
+const f2 = criarFuncionario('Maria', 11400, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+//Object.crate
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+//Uma função famosa que retorna Objeto
+const fromJson = JSON.parse('{"info": "Sou um JSON" }')
+console.log(fromJson)
