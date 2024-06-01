79be215b8ae50210b10b49c390d6fb2a7784edd5
Victor Ferreira <victorferreira9990@gmail.com>
2024-06-01 16:30:21 -0300

    herança

---
+++ poo/heranca.js
@@ -0,0 +1,12 @@
+const ferrari = {
+   modelo: 'F40',
+   velMax: 324
+}
+
+const volvo = {
+   modelo: 'V40',
+   velMAx: 200
+}
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
