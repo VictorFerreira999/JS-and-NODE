d93e0964edc4779f97ba591f0cab37d022d0de43
Victor Ferreira <victorferreira9990@gmail.com>
2024-07-08 09:01:22 -0300

    projeto API

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,15 +1,19 @@
 const porta = 3003
 
 const express =  require('express')
-const app= express()
+const app = express()
+const bancoDeDados = ('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-   console.log('Middleware 1...')
-   next()
+   res.send(bancoDeDados.getProdutos()) //converter para JSON
 })
 
-app.get('/produtos', (req, res, next) => {
-   res.send({ nome: 'Notebook', preco: 123.45 }) //converter para JSON
+app.get('/produtos/:id', (req, res, next) => {
+   res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+   const produto = bancoDeDados
 })
 
 app.listen(porta, () => {
